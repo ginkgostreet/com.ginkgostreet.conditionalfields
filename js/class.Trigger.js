99a01947e6010d031c8c15d71900f490cc6f5a6e
Adam Edison <aedison@ginkgostreet.com>
2016-01-18 15:09:48 -0500

    Refactoring determineInputType to belong to Trigger.

--- js/class.Trigger.js
+++ js/class.Trigger.js
@@ -5,7 +5,8 @@
  * e.g. cj("input#CIVICRM_QFID_0_34")
  * 
  * @param showValue string - value of the inputElement which causes the Trigger to evaluate to true
- * e.g. "1"
+ * e.g. "1" - typically for radio buttons or checkboxes to be "checked"
+ * e.g. undefined - for a radio button or checkbox to be "unchecked"
  * 
  * If per the above example, this were the only Trigger belonging to showHideObj, and the showValue at runtime = "1",
  * the showHideObj would be shown.
@@ -13,9 +14,11 @@
 function Trigger(inputElement, showValue) {
   this.inputElement = inputElement;
   this.showValue = showValue;
+  this.inputType = this.determineInputType();
 }
 /**
- * Simple property-setting function. Used during evaluation in other functions that user Triggers.
+ * If the Trigger itself is unable to properly determine the inputType during its construction,
+ * the inputType can be set manually by using this function.
  * 
  * @param inputType string - type of the inputElement
  * e.g. "checkbox"
@@ -24,4 +27,28 @@ function Trigger(inputElement, showValue) {
 Trigger.prototype.setInputType = function (inputType) {
     this.inputType = inputType;
 };
+
+/**
+ *
+ * Determines the type of the inputElement supplied.
+ * Defaults to 'value', or finds specific element by reflection:
+ * (e.g. 'radio', 'checkbox', 'multi-select', etc.)
+ * 
+ */
+Trigger.prototype.determineInputType = function() {
+  
+  var el = this.inputElement;
+  
+   if(el.is('select[multiple=multiple]'))
+     this.inputType = 'multi-select';
+   else if(el.is(':checkbox'))
+     this.inputType = 'checkbox';
+   else if(el.is(':radio'))
+     this.inputType = 'radio';
+   else
+     this.inputType = 'value';
+};
+
+// TODO have function to set new evaluation function, and have a default evaluation function. Remember that function are objects.
+// this.evaluateCondition
 
