517b7683cb10719be774793bc8a45becf5afff52
Adam Edison <aedison@ginkgostreet.com>
2016-02-04 12:18:53 -0500

    Added error handling and validation for showHideObj constructor.

--- js/class.showHideObj.js
+++ js/class.showHideObj.js
@@ -1,8 +1,31 @@
-function showHideObj(showHideElementsArrray) {
-  this.showHideElementsArray = showHideElementsArrray;
+function showHideObj(showHideElements) {
+  if (showHideElements instanceof Array) {
+    this.showHideElementsArray = showHideElements;
+  }
+  else if (showHideElements instanceof String) {
+    if (cj(showHideElements).length) {
+      this.showHideElementsArray = [];
+      this.showHideElementsArray.push(cj(showHideElements));
+    }
+    else {
+      throw new showHideConstructorError("Invalid constructor argument: " + showHideElements.toString());
+    }
+  }
+  else {
+    throw new showHideConstructorError("Invalid constructor argument. Requires string or valid jQuery selector, or array of jQuery selectors.");
+  }
+
   this.triggerObjectsArray = new Array;
   this.logicalOperator = 'AND';
 }
+
+function showHideConstructorError(message) {
+  this.message = message;
+  this.stack = (new Error()).stack;
+}
+
+showHideConstructorError.prototype = Object.create(Error.prototype);
+showHideConstructorError.prototype.name = "showHideConstructorError";
 
 showHideObj.prototype.setLogicalOperator = function(operatorString) {
   operatorString = operatorString.toUpperCase();
